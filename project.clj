5a3bb3ebcd296f5e155e5f8572e656245acf9030
Charles Pletcher <cpletcher2@gmail.com>
2015-04-30 17:09:31 -0700

    Increas repl timeout

--- project.clj
+++ project.clj
@@ -19,5 +19,6 @@
   :main ^:skip-aot slack-reporter.web
   :uberjar-name "slack-reporter.standalone.jar"
   :profiles {:uberjar {:aot :all}}
+  :repl-options {:timeout 120000}
   :ring {:handler slack-reporter.web/app})
 
